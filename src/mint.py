93a99d4517f92c9256615c6114908a0c896a7a51
thefulltimereader <thefulltimereader@gmail.com>
2010-09-26 04:54:19 -0400

    after incrementing the start with *2, also reuses the look up per outer loop

--- src/mint.py
+++ src/mint.py
@@ -16,7 +16,6 @@ Design a set of 5 coin denominations for US $ s.t.:
 'cost = [1e308]*100;'
 import time, math, sys
 
-n=0;
 
 def calcCost(cost):
     for i in range(2, 100):
@@ -29,13 +28,13 @@ def calcCost(cost):
               #      print "cost[",i,"] is now ", cost[i]
     return 0
 
-def getAvgCost(cost):
+def getAvgCost(cost, n):
     total = score=0.0
     for i in range(1, 100):
         total+= cost[i]
    #     print "# of coins for ", i," is ", cost[i];
         if cost[i] % 5 is 0:
-            score+=cost[i]*n
+            score+=cost[i]*int(n)
         else:
             score+=cost[i]
     print "total:",total,"avg cost:",(total/99) ,"\n score: ", score;
@@ -48,38 +47,37 @@ def getAvgCost(cost):
         i.e. if max{denom}<25, useless.
         Say B = 5, you must have a 20
 '''
-def tryAll():
+def tryAll(n):
     bestQuad = [1, 2, 3, 4, 5];
     best = (1e308, 0);
     cost = [1e308]*100;
     upperBound = math.ceil(99.0/5);
-    for i in range(2, 97):
-      #  cost[i:] = [1e308]*(100-i) #reuse the lookup table
-        for j in range(i+1, 98):
-       #     cost[j:]=[1e308]*(100-j)
-            for h in range(j+1, 99):
-        #        cost[h:]=[1e308]*(100-h)
-                for k in range(h+1, 100):
-         #           cost[k:]=[1e308]*(100-k)
+    for i in range(2, 70):
+        cost[i:] = [1e308]*(100-i) #reuse the lookup table
+        for j in range(i*2, 80):
+            cost[j:]=[1e308]*(100-j)
+            for h in range(j*2, 90):
+                cost[h:]=[1e308]*(100-h)
+                for k in range(h*2, 100):
+                    cost[k:]=[1e308]*(100-k)
                     cost = [1e308]*100;
                     tryQuad = [1, i, j, h, k];
                     if max(tryQuad)>=upperBound:
                         cost[1]=cost[i]=cost[j]=cost[h]=cost[k]=1;
                         calcCost(cost)
                         print "for ", tryQuad
-                        result = getAvgCost(cost)
+                        result = getAvgCost(cost, n)
                         if result[0] < best[0]:
                             best = result
                             bestQuad = tryQuad
-    print "the best score is: ", best[0], " with avg # of coins:", best[1]," with denomination: " ,bestQuad
+    print "With N=", n,"the best score is: ", best[0], " with avg # of coins:", best[1]," with denomination: " ,bestQuad
     
 def main():
  #   n = raw_input("What's the N?")
     n = sys.argv[1]
-    print "With N="+ n
     start = time.clock()
-    tryAll()
-    print "It took", (start-time.clock())," to complete"
+    tryAll(n)
+    print "It took", (time.clock()-start),"seconds to complete"
  
     
 if __name__ == '__main__':
